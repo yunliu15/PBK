3689fc8ac0d95d1aad9e5c54ab3437dbaa5b3c1f
sherodesigns5 <yun@sherodesigns.com>
2016-02-05 17:25:53 -0500

    setup quickview template, account page styling, product grid styling, search button

---
+++ primitivesbykathy.com/html/app/code/local/Amasty/Quickview/Helper/Product/View.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author Clayton
+ */
+
+class Amasty_Quickview_Helper_Product_View extends Mage_Catalog_Helper_Product_View
+{
+    public function initProductLayout($product, $controller)
+    {
+        $design = Mage::getSingleton('catalog/design');
+        $settings = $design->getDesignSettings($product);
+
+        if ($settings->getCustomDesign()) {
+            $design->applyCustomDesign($settings->getCustomDesign());
+        }
+
+        $update = $controller->getLayout()->getUpdate();
+        $update->addHandle('default');
+        $controller->addActionLayoutHandles();
+
+        $update->addHandle('PRODUCT_TYPE_' . $product->getTypeId());
+        $update->addHandle('PRODUCT_' . $product->getId());
+        /**
+         * This is the only modification to this file. IF using amquickview, using this handle found in catalog.xml
+         */
+        if (Mage::app()->getRequest()->getModuleName() === 'amquickview') {
+            $update->addHandle('PRODUCT_QUICKVIEW');
+        }
+        $controller->loadLayoutUpdates();
+
+        // Apply custom layout update once layout is loaded
+        $layoutUpdates = $settings->getLayoutUpdates();
+        if ($layoutUpdates) {
+            if (is_array($layoutUpdates)) {
+                foreach($layoutUpdates as $layoutUpdate) {
+                    $update->addUpdate($layoutUpdate);
+                }
+            }
+        }
+
+        $controller->generateLayoutXml()->generateLayoutBlocks();
+
+        // Apply custom layout (page) template once the blocks are generated
+        if ($settings->getPageLayout()) {
+            $controller->getLayout()->helper('page/layout')->applyTemplate($settings->getPageLayout());
+        }
+
+        $currentCategory = Mage::registry('current_category');
+        $root = $controller->getLayout()->getBlock('root');
+        if ($root) {
+            $controllerClass = $controller->getFullActionName();
+            if ($controllerClass != 'catalog-product-view') {
+                $root->addBodyClass('catalog-product-view');
+            }
+            $root->addBodyClass('product-' . $product->getUrlKey());
+            if ($currentCategory instanceof Mage_Catalog_Model_Category) {
+                $root->addBodyClass('categorypath-' . $currentCategory->getUrlPath())
+                    ->addBodyClass('category-' . $currentCategory->getUrlKey());
+            }
+        }
+
+        return $this;
+    }
+}
