238371c20300975057267d6157c66516e07eb159
sherodesigns5 <yun@sherodesigns.com>
2016-03-18 17:36:26 -0400

    fixed js error for product grid, fixing styles according to the new list

--- primitivesbykathy.com/html/skin/frontend/rwd/pbk/js/app.js
+++ primitivesbykathy.com/html/skin/frontend/rwd/pbk/js/app.js
@@ -869,86 +869,7 @@ $j(document).ready(function () {
         });
     }
 
-    // ==============================================
-    // UI Pattern - Toggle Content (tabs and accordions in one setup)
-    // ==============================================
-    
-    $j('.toggle-content').each(function () {
-        var wrapper = jQuery(this);
 
-        var hasTabs = wrapper.hasClass('tabs');
-        var hasAccordion = wrapper.hasClass('accordion');
-        var startOpen = wrapper.hasClass('open');
-
-        var dl = wrapper.children('dl:first');
-        var dts = dl.children('dt');
-        var panes = dl.children('dd');
-        var groups = new Array(dts, panes);
-
-        //Create a ul for tabs if necessary.
-        if (hasTabs) {
-            var ul = jQuery('<ul class="toggle-tabs"></ul>');
-            dts.each(function () {
-                var dt = jQuery(this);
-                var li = jQuery('<li></li>');
-                li.html(dt.html());
-                ul.append(li);
-            });
-            ul.insertBefore(dl);
-            var lis = ul.children();
-            groups.push(lis);
-        }
-
-        //Add "last" classes.
-        var i;
-        for (i = 0; i < groups.length; i++) {
-            groups[i].filter(':last').addClass('last');
-        }
-
-        function toggleClasses(clickedItem, group) {
-            var index = group.index(clickedItem);
-            var i;
-            for (i = 0; i < groups.length; i++) {
-                groups[i].removeClass('current');
-                groups[i].eq(index).addClass('current');
-            }
-        }
-
-        //Toggle on tab (dt) click.
-        dts.on('click', function (e) {
-            //They clicked the current dt to close it. Restore the wrapper to unclicked state.
-            if (jQuery(this).hasClass('current') && wrapper.hasClass('accordion-open')) {
-                wrapper.removeClass('accordion-open');
-            } else {
-                //They're clicking something new. Reflect the explicit user interaction.
-                wrapper.addClass('accordion-open');
-            }
-            toggleClasses(jQuery(this), dts);
-
-        });
-
-        //Toggle on tab (li) click.
-        if (hasTabs) {
-            lis.on('click', function (e) {
-                toggleClasses(jQuery(this), lis);
-
-                //for homepage featured items
-                if ($j('.featured-items').length) {
-                    alignProductGridActions();
-                }
-
-            });
-            //Open the first tab.
-            lis.eq(0).trigger('click');
-
-        }
-
-        //Open the first accordion if desired.
-        if (startOpen) {
-            dts.eq(0).trigger('click');
-        }
-
-    });
 
 
     // ==============================================
@@ -1143,6 +1064,95 @@ $j(document).ready(function () {
             alignProductGridActions();
         });
     }
+
+
+// original position: line 936. have to move this section down here to fix the error alignProductGridActions is not a function       ------------ 031816 yl
+
+    // ==============================================
+    // UI Pattern - Toggle Content (tabs and accordions in one setup)
+    // ==============================================
+
+    $j('.toggle-content').each(function () {
+        var wrapper = jQuery(this);
+
+        var hasTabs = wrapper.hasClass('tabs');
+        var hasAccordion = wrapper.hasClass('accordion');
+        var startOpen = wrapper.hasClass('open');
+
+        var dl = wrapper.children('dl:first');
+        var dts = dl.children('dt');
+        var panes = dl.children('dd');
+        var groups = new Array(dts, panes);
+
+        //Create a ul for tabs if necessary.
+        if (hasTabs) {
+            var ul = jQuery('<ul class="toggle-tabs"></ul>');
+            dts.each(function () {
+                var dt = jQuery(this);
+                var li = jQuery('<li></li>');
+                li.html(dt.html());
+                ul.append(li);
+            });
+            ul.insertBefore(dl);
+            var lis = ul.children();
+            groups.push(lis);
+        }
+
+        //Add "last" classes.
+        var i;
+        for (i = 0; i < groups.length; i++) {
+            groups[i].filter(':last').addClass('last');
+        }
+
+        function toggleClasses(clickedItem, group) {
+            var index = group.index(clickedItem);
+            var i;
+            for (i = 0; i < groups.length; i++) {
+                groups[i].removeClass('current');
+                groups[i].eq(index).addClass('current');
+            }
+        }
+
+        //Toggle on tab (dt) click.
+        dts.on('click', function (e) {
+            //They clicked the current dt to close it. Restore the wrapper to unclicked state.
+            if (jQuery(this).hasClass('current') && wrapper.hasClass('accordion-open')) {
+                wrapper.removeClass('accordion-open');
+            } else {
+                //They're clicking something new. Reflect the explicit user interaction.
+                wrapper.addClass('accordion-open');
+            }
+            toggleClasses(jQuery(this), dts);
+
+        });
+
+        //Toggle on tab (li) click.
+        if (hasTabs) {
+            lis.on('click', function (e) {
+                toggleClasses(jQuery(this), lis);
+
+                //for homepage featured items
+                if ($j('.featured-items').length) {
+                    alignProductGridActions();
+                }
+
+            });
+            //Open the first tab.
+            lis.eq(0).trigger('click');
+
+        }
+
+        //Open the first accordion if desired.
+        if (startOpen) {
+            dts.eq(0).trigger('click');
+        }
+
+    });
+
+
+
+
+
 
     // ==============================================
     // Generic, efficient window resize handler
